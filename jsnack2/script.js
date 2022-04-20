7a6ab6930a4945443e4f7f71d7dce3f96d2c4938
dmonteleo <dmonteleo@gmail.com>
2022-04-20 21:03:18 +0200

    snack 3 done

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,46 +3,7 @@
  L’utente inserisce due parole in successione, con due prompt.
  Il software stampa prima la parola più corta, poi la parola più lunga.
 */
-/*
- JSnack 3
- Il software deve chiedere per 5 volte all’utente di inserire un numero.
- Il programma stampa la somma di tutti i numeri inseriti.
- Esegui questo programma in due versioni, con il for e con il while (facoltativo)
-*/
-/*
- JSnack 4
- In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
- chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
-*/
-/*
- JSnack 5
- Crea un array vuoto.
- Chiedi per 6 volte all’utente di inserire un numero,
- se è dispari inseriscilo nell’array
-*/
-/*
- JSnack 6
- Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
-*/
-/*
- JSnack 7
- Stampa le potenze di 2 fino a 1000.
- questo snack cela un trabocchetto:  1000 non è una potenza di 2
- perciò per fermarsi prima di 1000 bisogna stampare la potenza prima di incrementarla
- perché altrimenti il controllo viene fatto dopo e si sfora
-*/
-/*
- JSnack 8
- Chiedi un numero di 4 cifre all’utente
- e calcola la somma di tutte le cifre che compongono il numero.
- in questo caso possiamo sfruttare a nostro vantaggio il fatto che
- la funzione prompt restituisca una stringa anche se si è digitato un numero
- e recuperare una cifra alla volta usanto il ciclo for per scorrere la stringa numerica
-*/
-/*
- JSnack 9
- Calcola la somma e la media dei primi 10 numeri.
-*/
+
 
 let firstWord;
 do {
