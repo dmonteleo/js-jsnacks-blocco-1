552052e69d682ef09e4d74c605167d2748f4ed95
dmonteleo <dmonteleo@gmail.com>
2022-04-21 15:49:26 +0200

    exercise 5 done

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -13,26 +13,3 @@ for ( i=1; i<=6; i++) {
 console.log(array);
 
 
-/*
- JSnack 6
- Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
-*/
-/*
- JSnack 7
- Stampa le potenze di 2 fino a 1000.
- questo snack cela un trabocchetto:  1000 non è una potenza di 2
- perciò per fermarsi prima di 1000 bisogna stampare la potenza prima di incrementarla
- perché altrimenti il controllo viene fatto dopo e si sfora
-*/
-/*
- JSnack 8
- Chiedi un numero di 4 cifre all’utente
- e calcola la somma di tutte le cifre che compongono il numero.
- in questo caso possiamo sfruttare a nostro vantaggio il fatto che
- la funzione prompt restituisca una stringa anche se si è digitato un numero
- e recuperare una cifra alla volta usanto il ciclo for per scorrere la stringa numerica
-*/
-/*
- JSnack 9
- Calcola la somma e la media dei primi 10 numeri.
-*/
